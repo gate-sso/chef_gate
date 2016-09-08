44dbfaa717f9c8987e34720083e941eaea41e998
Sankalp Singh <singh.sankalp1@gmail.com>
2016-09-08 14:45:35 +0530

    [Sankalp] fixing chef client run

--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -20,3 +20,13 @@ chef_gate_pam 'ssh setup' do
   api_key node['gate']['nss']['api_key']
 end
 
+cookbook_file "/etc/nsswitch.conf.orig" do
+  source "nsswitch.conf.orig"
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
+if node['platform'] == 'ubuntu' && node['platform_version'] == '14.04'
+  Chef::Util::FileEdit.new('/usr/bin/chef-client').insert_line_after_match(/embedded/, "require 'fileutils'\nFileUtils.cp '/etc/nsswitch.conf.orig', '/etc/nsswitch.conf'")
+end
