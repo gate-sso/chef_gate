6af3de5e02bda03da0ead35136dab4305264ae3f
Sankalp Singh <singh.sankalp1@gmail.com>
2016-10-12 16:40:21 +0530

    [Sankalp] api access on public ul

--- recipes/_add_host_gate.rb
+++ recipes/_add_host_gate.rb
@@ -2,11 +2,5 @@ require 'net/http'
 require 'socket'
 require 'json'
 
-uri = URI("https://#{node['gate']['url']}/nss/host")
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl?
-request = Net::HTTP::Post.new(uri.request_uri)
-request.set_form_data({"name" => "#{node['hostname']}", "token" => "#{node['gate']['nss']['api_key']}"})
-
-res = http.request(request)
+uri = URI("https://#{node['gate']['public_url']}/nss/host")
+res = Net::HTTP.post_form(uri,"name" => "#{node['hostname']}", "token" => "#{node['gate']['nss']['api_key']}")
--- recipes/_add_sudo_group.rb
+++ recipes/_add_sudo_group.rb
@@ -2,14 +2,10 @@ require 'net/http'
 require 'socket'
 require 'json'
 
-uri = URI("https://#{node['gate']['url']}/nss/host")
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl?
+uri = URI("https://#{node['gate']['public_url']}/nss/host")
 params = { :name => "#{node['hostname']}", :token => "#{node['gate']['nss']['api_key']}" }
 uri.query = URI.encode_www_form(params)
-res = http.request(Net::HTTP::Get.new(uri.request_uri))
-#res = Net::HTTP.get_response(uri)
+res = Net::HTTP.get_response(uri)
 
 response_hash = JSON.parse(res.body)
 
